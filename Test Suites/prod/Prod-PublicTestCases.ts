3b6ee7403907b797f022848b44e8a7e67fcec38a
nilmini <nilmini@PABLO.genesiis.com>
2020-04-17 17:05:22 +0530

    202004 1705 NA folder rename

---
+++ Test Suites/prod/Prod-PublicTestCases.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>All public Test cases will be there</description>
+   <name>Prod-PublicTestCases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2746eb3d-6923-421b-8fcf-c9aedcb9ffb6</testSuiteGuid>
+   <testCaseLink>
+      <guid>45c3ea78-98c3-4cfc-bd3b-519073a32bcd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/prod_test_cases/TC-TJPR-0001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6db9fd09-47f6-4a0f-8014-a768e0df1cbb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/prod_test_cases/TC-TJPR-0003</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
